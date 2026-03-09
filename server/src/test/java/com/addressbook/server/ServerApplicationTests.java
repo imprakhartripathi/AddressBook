3b8c0cd8d7bc8ea0db51181fca277319bececf93
Prakhar Tripathi <imprakhartripathiofficial@gmail.com>
2026-03-09 14:10:29 +0530

    project init

---
+++ server/src/test/java/com/addressbook/server/ServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.addressbook.server;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ServerApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
